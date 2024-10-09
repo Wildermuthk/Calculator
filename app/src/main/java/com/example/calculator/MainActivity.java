a183c4864a65e2fc189aa3c1373d18724403582c
Wildermuthk <Wildermuthk@cwu.edu>
2024-10-08 23:36:03 -0700

    Assigned id's to all buttons and started adding functionality to the buttons.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+   TextView resultTV, solutionTV;
+
+   MaterialButton buttonC, buttonParOpen, buttonParClose;
+
+   MaterialButton button0, button1, button2, button3,
+           button4, button5, button6, button7, button8,  button9;
+
+   MaterialButton buttonMul, buttonDivide, buttonPlus, buttonMinus, buttonEquals;
+
+   MaterialButton buttonAC, buttonDot;
+
+    /**
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +42,76 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        resultTV = findViewById(R.id.result_tv);
+
+        solutionTV = findViewById(R.id.solution_tv);
+
+        assignID(buttonC, R.id.button_c);
+        assignID(buttonParOpen, R.id.button_open_parenthesis);
+        assignID(buttonParClose, R.id.button_close_parenthesis);
+
+        assignID(button0, R.id.button_0);
+        assignID(button1, R.id.button_1);
+        assignID(button2, R.id.button_2);
+        assignID(button3, R.id.button_3);
+        assignID(button4, R.id.button_4);
+        assignID(button5, R.id.button_5);
+        assignID(button6, R.id.button_6);
+        assignID(button7, R.id.button_7);
+        assignID(button8, R.id.button_8);
+        assignID(button9, R.id.button_9);
+
+        assignID(buttonMul, R.id.button_multiply);
+        assignID(buttonDivide, R.id.button_division);
+        assignID(buttonPlus, R.id.button_plus);
+        assignID(buttonMinus, R.id.button_minus);
+
+        assignID(buttonAC, R.id.button_ac);
+        assignID(buttonDot, R.id.button_dot);
+        assignID(buttonEquals, R.id.button_equals);
+
+    }
+
+
+    /**
+     *
+     * @param btn
+     * @param id
+     */
+    void assignID(MaterialButton btn, int id){
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+    /**
+     *
+     * @param view The view that was clicked.
+     */
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
+        String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTV.getText().toString();
+
+        if(buttonText.equals("AC")){
+            solutionTV.setText("");
+            resultTV.setText("0");
+            return;
+        }
+        if(buttonText.equals("=")){
+            solutionTV.setText(resultTV.getText());
+            return;
+        }
+        if(buttonText.equals("C")){
+            dataToCalculate = dataToCalculate.substring(0,dataToCalculate.length()-1);
+        } else {
+            dataToCalculate = dataToCalculate + buttonText;
+        }
+
+        solutionTV.setText(dataToCalculate);
+
+        //String finalResult = getResults();
+
     }
 }
